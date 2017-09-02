48c1e0be6b9ebd6b6b2d98aafe9d895a60899a32
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2017-09-02 11:43:25 +0300

    Empty project with connected DB

---
+++ src/main/java/com/projectstarter/ProjectStarter/ProjectStarterApplication.java
@@ -0,0 +1,12 @@
+package com.projectstarter.ProjectStarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectStarterApplication.class, args);
+	}
+}
