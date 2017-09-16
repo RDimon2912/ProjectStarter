f5533a5c87cd50b307d2bce7f19906923c3ab62f
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2017-09-16 16:31:04 +0300

    Set default image when create project

--- src/main/java/com/projectstarter/ProjectStarter/ProjectStarterApplication.java
+++ src/main/java/com/projectstarter/ProjectStarter/ProjectStarterApplication.java
@@ -3,9 +3,11 @@ package com.projectstarter.ProjectStarter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @Import({ SecurityConfiguration.class, WebConfiguration.class })
+@PropertySource({"classpath:application.properties"})
 public class ProjectStarterApplication {
 
 	public static void main(String[] args) {
