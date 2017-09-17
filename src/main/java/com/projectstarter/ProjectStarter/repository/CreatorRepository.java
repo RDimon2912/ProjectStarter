6b6687ad4902007b5d4da2dfa6beac79d6329ace
LisavetaTsyulia <tsyulialisa@gmail.com>
2017-09-17 10:36:34 +0300

    Admin confirms or dismisses new creator, but some problems with observable

--- src/main/java/com/projectstarter/ProjectStarter/repository/CreatorRepository.java
+++ src/main/java/com/projectstarter/ProjectStarter/repository/CreatorRepository.java
@@ -5,4 +5,5 @@ import com.projectstarter.ProjectStarter.model.CreatorRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CreatorRepository extends JpaRepository<CreatorRequest, Long> {
+    public CreatorRequest findByUser(Long id);
 }
---
+++ src/main/java/com/projectstarter/ProjectStarter/service/dto/admin/ResponseScanDto.java
@@ -0,0 +1,11 @@
+package com.projectstarter.ProjectStarter.service.dto.admin;
+
+import com.projectstarter.ProjectStarter.service.dto.Dto;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ResponseScanDto implements Dto {
+    public String passportScan;
+}
