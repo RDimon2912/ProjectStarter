c0291c1cdb3255a457f46882bd704bbd75d63f1c
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2017-09-20 09:33:51 +0300

    Addede classes for search and Indexed annotation to Project class

--- src/main/java/com/projectstarter/ProjectStarter/model/Project.java
+++ src/main/java/com/projectstarter/ProjectStarter/model/Project.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.search.annotations.Indexed;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
@@ -16,6 +17,7 @@ import java.util.List;
 @Getter
 @Setter
 @NoArgsConstructor
+@Indexed
 public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
---
+++ src/main/java/com/projectstarter/ProjectStarter/repository/fulltextsearch/SearchIndexBuilder.java
@@ -0,0 +1,29 @@
+package com.projectstarter.ProjectStarter.repository.fulltextsearch;
+
+import org.hibernate.search.jpa.FullTextEntityManager;
+import org.hibernate.search.jpa.Search;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+@Component
+public class SearchIndexBuilder implements ApplicationListener<ApplicationReadyEvent> {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Transactional
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent applicationReadyEvent) {
+        try {
+            FullTextEntityManager fullTextEntityManager =
+                    Search.getFullTextEntityManager(entityManager);
+            fullTextEntityManager.createIndexer().startAndWait();
+        } catch (InterruptedException ex) {
+            System.out.println("An error occurred trying to build the serach index: " + ex.toString());
+        }
+    }
+}
